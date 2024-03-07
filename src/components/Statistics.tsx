278c31807ec45044e138e60decb8b56076f3c67b
Karol Koś <kosiyyu@gmail.com>
2024-03-08 00:18:36 +0100

    Refactor Statistics

--- src/components/Statistics.tsx
+++ src/components/Statistics.tsx
@@ -4,26 +4,26 @@ import { View, Text, FlatList } from 'react-native';
 import { useSQLiteContext } from 'expo-sqlite/next';
 import { ensureTwoDigits } from '@utils/calendar';
 import { FetchedDay } from '@constants/types';
-import { LineChart } from "react-native-chart-kit";
 import { colors, sizes, fonts } from '@constants/styles';
 import { days } from '@constants/daysMonths';
-import { AbstractChartConfig } from 'react-native-chart-kit/dist/AbstractChart';
+import LineChart, { Point } from '@components/LineChart';
 
 const Statistics: React.FC = () => {
   const db = useSQLiteContext();
   const [currentDate, setCurrentDate] = useState(new Date());
-  const [weeklyActivityData, setWeeklyActivityData] = useState<FetchedDay[]>([]);
+  const [weeklyActivityData, setWeeklyActivityData] = useState<Point[]>([]);
 
   useEffect(() => {
-    weeklyActivity()
-    .then((data) => {
-      setWeeklyActivityData(data);
-    }).catch((error) => {
-      console.log('error weeklyActivity', error);
-    });
+    generate()
+      .then((data) => {
+        setWeeklyActivityData(data);
+        console.log('data', data);
+      }).catch((error) => {
+        console.log('error generate', error);
+      });
   }, []);
 
-  const weeklyActivity = async () => {
+  const generate = async (): Promise<Point[]> => {
     const dayOfWeek = (currentDate.getDay() + 6) % 7;
     const startOfWeek = new Date(currentDate.setDate(currentDate.getDate() - dayOfWeek));
     const endOfWeek = new Date(currentDate.setDate(startOfWeek.getDate() + 6));
@@ -38,7 +38,7 @@ const Statistics: React.FC = () => {
 
     const dateFrom = `${startOfWeek.getFullYear()}-${ensureTwoDigits(startOfWeek.getMonth() + 1)}-${ensureTwoDigits(startOfWeek.getDate())}`;
     const dateTo = `${endOfWeek.getFullYear()}-${ensureTwoDigits(endOfWeek.getMonth() + 1)}-${ensureTwoDigits(endOfWeek.getDate())}`;
-  
+
     try {
       await db.withExclusiveTransactionAsync(async (txn) => {
         const fetchedDays: FetchedDay[] = await txn.getAllAsync('SELECT * FROM mood_entery WHERE moodDate >= ? AND moodDate <= ?;', [dateFrom, dateTo]);
@@ -54,70 +54,83 @@ const Statistics: React.FC = () => {
       console.log('error generatedDaysNEW', error);
     }
 
-    return days;
+    // brute
+    const result = days.map((day, index) => {
+      return { x: index, y: day.mood };
+    });
+
+    return result;
   };
 
-  const screenWidth = Dimensions.get('window').width;
+  const data: Point[] = [
+    { x: 0, y: 0 },
+    { x: 1, y: 1 },
+    { x: 2, y: 2 },
+    { x: 3, y: 0 },
+    { x: 4, y: 4 },
+    { x: 5, y: 5 },
+    { x: 6, y: 4 },
+  ];
 
-  const data = {
-    labels: days,
-    datasets: [
-      {
-        data: [1, 0, 3, 5, 4, 1, 2],
-        strokeWidth: 1,
-      }
-    ],
-    legend: ['Mood level'],
-  };
-  
-  const chartConfig: AbstractChartConfig = {
-    decimalPlaces: 0,
-    color: () => colors.specialLight,
-    labelColor: () => colors.white,
-    style: {
-      borderRadius: 16,
-    },
-    propsForDots: {
-      r: '3',
-      strokeWidth: '0',
-      stroke: colors.white
-    },
-  };
+  const xLabel = days;
+  const yLabel = ['0', '1', '2', '3', '4', '5'];
+
+  const width = Dimensions.get('window').width - 70;
+  const height = Dimensions.get('window').width - 70 - 70;
 
   return (
     <View
       style={{
         flex: 1,
+        alignItems: 'center',
         backgroundColor: colors.black,
       }}
     >
-      <View>
-        <View 
+      <View
+        style={{
+          alignItems: 'center',
+          backgroundColor: colors.secondary,
+          padding: sizes.medium,
+          borderRadius: sizes.medium,
+          margin: sizes.medium,
+        }}
+      >
+        <Text
           style={{
-            alignItems: 'center',
-            backgroundColor: colors.black,
+            color: colors.white,
+            fontFamily: fonts.bold,
+            fontSize: sizes.medium,
           }}
         >
+          Weekly Activity
+        </Text>
+        {weeklyActivityData.length > 0 && (
           <LineChart
-            data={data}
-            width={screenWidth}
-            height={256}
-            chartConfig={chartConfig}
-            bezier
-          />
-          {/* <FlatList
             data={weeklyActivityData}
-            keyExtractor={(item, index) => index.toString()}
-            renderItem={({ item }) => (
-              <View>
-                <Text>{item.moodDate}</Text>
-                <Text>{item.mood}</Text>
-              </View>
-            )}
-          /> */}
-        </View>
-      </View>
+            xLabel={xLabel}
+            yLabel={yLabel}
 
+            lineWidth={2}
+            width={width}
+            height={height}
+
+            backgroundColor={colors.secondary}
+            xLineColor={colors.white}
+            yLineColor={colors.white}
+            lineColor={colors.specialLight}
+            gradientStartColor={colors.specialLight}
+            gradientEndColor={colors.specialLight}
+            gridColor={colors.white}
+            xLabelTextColor={colors.white}
+            yLabelTextColor={colors.white}
+
+            gradientStartOpacity='0.5'
+            gradientEndOpacity='0'
+
+            displayGrid={false}
+          />
+        )}
+      </View>
     </View>
   );
 };
